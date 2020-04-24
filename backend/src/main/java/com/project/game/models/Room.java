53a7c4c73182acddb42bfa23931e8076d3972976
havyaOYO <havyakumar.panchal@oyorooms.com>
2020-04-24 18:44:51 +0530

    [added] db-connection done

---
+++ backend/src/main/java/com/project/game/models/Room.java
@@ -0,0 +1,41 @@
+package com.project.game.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+/**
+ * Created by havyapanchal on 24 Apr, 2020 , 1:07 PM
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Room {
+    @Id
+    private String id ;
+    @JsonProperty("room_code")
+    @SerializedName("room_code")
+    private String roomCode ;
+    //TODO: Is roomName necessary ?
+    @JsonProperty("room_name")
+    @SerializedName("room_name")
+    private String roomName ;
+    @JsonProperty("player_info")
+    @SerializedName("player_info")
+    private List<PlayerInfo> playersInfo ;
+    @JsonProperty("no_of_rounds")
+    @SerializedName("no_of_rounds")
+    private Integer noOfRounds ;
+}
+
+
