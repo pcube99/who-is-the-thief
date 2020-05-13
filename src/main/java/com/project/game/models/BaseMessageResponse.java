be3cd9b0ead7524efe063f4631e18564f5f899ea
havya-1310 <hkpanchal1998@gmail.com>
2020-05-14 00:00:29 +0530

    :art:

---
+++ src/main/java/com/project/game/models/BaseMessageResponse.java
@@ -0,0 +1,14 @@
+package com.project.game.models;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class BaseMessageResponse extends ResponseEntity {
+    public BaseMessageResponse(final Object message, final HttpStatus status) {
+        super(message, status);
+    }
+
+    public BaseMessageResponse(final Object message) {
+        super(message, HttpStatus.OK);
+    }
+}
